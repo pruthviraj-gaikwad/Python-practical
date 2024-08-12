cbad404e699156a7863d9e40fab57e99672cc238
pruthviraj-gaikwad <141212098+pruthviraj-gaikwad@users.noreply.github.com>
2024-08-12 21:23:56 +0530

    False_version_updated

--- parc_4/count_method.py
+++ parc_4/count_method.py
@@ -1,33 +1,43 @@
-def count(ms,s,b):
+def count(original_string,maching_string,b):
 	c =0
 	a =[]
-	if(s==''):
-		return 
-	if s==ms:
+	bool=False
+	if(maching_string==''):
+		for i in range(len(original_string)):
+			if(original_string[0]==''):
+				bool=True 
+				c+=1
+			else:
+				continue
+		if(bool==True):
+			return c
+		else:
+			return len(original_string)+1
+			
+	if maching_string==original_string:
 		return 1
+
 	if(b==True):
-		for i in range(len(ms)):
-			for j in range(len(ms)):
-				a.append(ms[i:j+1])
+		for i in range(len(original_string)):
+			for j in range(len(original_string)):
+				a.append(original_string[i:j+1])
 
 		for i in range(len(a)):
-			if(a[i]==s):
+			if(a[i]==maching_string):
 				c+=1
 	else:
 		s1=''
 		d =0
 		c =0
-		for i in range(len(ms)-len(s)):
+		for i in range(len(original_string)-len(maching_string)):
 			s1=''
-			i=d
-			if(ms[i]==s[0]):
-				for j in range(i,i+len(s)):
-					s1+=ms[j]
-			if s==s1:
+			if(original_string[i]==s[0]):
+				for j in range(i,i+len(maching_string)):
+					s1+=original_string[j]
+			if maching_string==s1:
 				c+=1
-				d = j+1
+				i = j+1
 			else:
-				d = i+1
 				continue
 
 	return c
